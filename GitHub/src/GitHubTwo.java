c16f853848d866ec04b54ccf97b54a4abf705078
sures <sures@LAPTOP-8GGFC4AH>
2022-06-21 23:03:16 +0530

    GitHub

---
+++ GitHub/src/GitHubTwo.java
@@ -0,0 +1,13 @@
+
+public class GitHubTwo {
+
+c          int a = 17;
+            int b = 117;
+         int num(int a,int b) {
+		int total=(a*b);
+		
+		
+	}
+		
+				
+}
---
+++ GitHub/src/ManualTesting/GitHubTwo.java
@@ -0,0 +1,28 @@
+package ManualTesting;
+
+
+
+public class GitHubTwo {
+
+
+          int a = 17;
+            int b = 117;
+            
+         int num(int a,int b) {
+		int total= a*b;
+		return total;
+         }
+		public static void main(String[] args) {
+			GitHubTwo gt = new GitHubTwo();
+			
+		int	result = gt.num(17, 117);
+		System.out.println("values :" + result);
+		}
+		
+	}
+		
+				
+
+
+
+
